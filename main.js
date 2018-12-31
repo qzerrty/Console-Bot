61ad694931c0c83285a646ec7dc8f7eea0ece8c1
Alex <qzerrty@protonmail.com>
2018-12-31 16:20:13 +0300

    PWA Upgrade

--- main.js
+++ main.js
@@ -185,4 +185,16 @@ element.parentNode.parentNode.childNodes[4].style.display='none'
 else element.parentNode.parentNode.childNodes[4].style.display='block'
 sP.consol.scrollTop=sP.field.offsetTop
 sP.enter.style.top=sP.field.offsetTop+'px'}
-if(navigator.serviceWorker.controller){console.log('[PWA Builder] active service worker found, no need to register')}else{navigator.serviceWorker.register('pwabuilder-sw.js',{scope:'./'}).then(function(reg){console.log('Service worker has been registered for scope:'+reg.scope)})}
+//This is the "Offline copy of pages" service worker
+
+//Add this below content to your HTML page, or add the js file to your page at the very top to register service worker
+if (navigator.serviceWorker.controller) {
+  console.log('[PWA Builder] active service worker found, no need to register')
+} else {
+  //Register the ServiceWorker
+  navigator.serviceWorker.register('pwabuilder-sw.js', {
+    scope: './'
+  }).then(function(reg) {
+    console.log('Service worker has been registered for scope:'+ reg.scope);
+  });
+}
